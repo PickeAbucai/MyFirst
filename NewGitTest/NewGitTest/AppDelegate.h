44ec1cdbe511ecbdb8fa1c248ea7d4f83f607c4c
lmj <lmj@itcast.cn>
2015-07-06 10:40:48 +0800

    初始化项目

---
+++ NewGitTest/NewGitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NewGitTest
+//
+//  Created by qianfeng00 on 15-7-6.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewGitTest/NewGitTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NewGitTest
+//
+//  Created by qianfeng00 on 15-7-6.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
